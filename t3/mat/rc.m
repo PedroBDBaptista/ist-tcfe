8f0e85189c800430ce38d29cbd8e1e80ef128feb
mgvaz <mariovaz@tecnico.ulisboa.pt>
2021-05-02 13:21:37 +0100

    a

--- t3/mat/rc.m
+++ t3/mat/rc.m
@@ -1,74 +1,68 @@
 close all
 clear all
 
-%%EXAMPLE SYMBOLIC COMPUTATIONS
+dt=1e-5
+tmax=1e4
+freq=2*pi*50.;
+period=1./50.
+A=230./10.;
 
-pkg load symbolic
-
-syms t
-syms R
-syms C
-syms vi(t)
-syms vo(t)
-syms i(t)
-
-i(t)=C*diff(vo,t)
-
-printf("\n\nKVL equation:\n");
-
-vi(t) = R*i(t)+vo(t)
-
-syms vo_n(t) %natural solution
-syms vo_f(t) %forced solution
-
-printf("\n\nSolution is of the form");
-
-v(t) = vo_n(t) + vo_f(t)
-
-printf("\n\nNatural solution:\n");
-syms A
-syms wn
-
-vi(t) = 0 %no excitation
-i_n(t) = C*diff(vo_n, t)
+%envelope
+R1=5e3;
+C1=1;
 
 
-printf("\n\n Natural solution is of the form");
-vo_n(t) = A*exp(wn*t)
 
-R*i_n(t)+vo_n(t) == 0
-
-R*C*wn*vo_n(t)+vo_n(t) == 0
-
-R*C*wn+1==0
-
-solve(ans, wn)
+% T off
+toff=(1/freq)*atan(R1*C1*freq)
 
 
-%%EXAMPLE NUMERIC COMPUTATIONS
+% T on
+f=@(x) sin(freq*x)-sin(freq*toff)*exp(-(x-toff)/(R1*C1));
+x0=toff;
+ton=fzero(f,x0)
 
-R=1e3 %Ohm
-C=100e-9 %F
+condition=1;
 
-f = 1000 %Hz
-w = 2*pi*f; %rad/s
 
-%time axis: 0 to 10ms with 1us steps
-t=0:1e-6:10e-3; %s
+for i=1:tmax
+	t(i)=i*dt;
+	v2(i)=A*sin(freq*t(i));
 
-Zc = 1/(j*w*C)
-Cgain = Zc/(R+Zc)
-Gain = abs(Cgain)
-Phase = angle(Cgain)
+	if(v2(i)>0)
+		v3(i)=v2(i);
+	else
+		v3(i)=0.;
+	endif;
 
-vi = 1*cos(w*t);
-vo = Gain*cos(w*t+Phase);
+	if(t(i)>toff && t(i)<ton)
+		vo(i)=A * sin(freq*toff) * exp(-(t(i)-toff)/(R1*C1));
+		condition=0;
+	else
+		vo(i)=0.;
+	endif;
+
+	if(condition==0)
+		toff=toff+period;
+		ton=ton+period;
+		condition=1;
+	endif;
+
+endfor;
+
+
+
+
+
 
 hf = figure ();
-plot (t*1000, vi, "g");
+plot (t, v2, "g");
 hold on;
-plot (t*1000, vo, "b");
+plot (t, v3, "r");
+hold on;
+plot (t, vo, "b");
 
-xlabel ("t[ms]");
-ylabel ("vi(t), vo(t) [V]");
-print (hf, "forced.eps", "-depsc");
+
+xlabel ("t[s]");
+ylabel ("v2(t) [V]");
+print (hf, "v2.eps", "-depsc");
