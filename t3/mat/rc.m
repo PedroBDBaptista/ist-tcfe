313aea4bc3c1af1facb4e670c42cbbb825b10ee0
PedroBDBaptista <pedroduartebaptista@tecnico.ulisboa.pt>
2021-05-03 00:56:34 +0100

    alteracoes

--- t3/mat/rc.m
+++ t3/mat/rc.m
@@ -2,7 +2,7 @@ close all
 clear all
 
 dt=1e-5
-tmax=1e4
+tmax=2e4+2000;
 freq=2*pi*50.;
 period=1./50.
 A=230./10.;
@@ -10,12 +10,13 @@ A=230./10.;
 
 %envelope
 R1=300e3;
+R2=715e3;
 C1=300e-6;
 
 
 
 % T off
-toff=(1/freq)*atan(R1*C1*freq)
+toff=(1/freq)*atan(R1*C1*freq);
 
 
 j=1;
@@ -50,21 +51,71 @@ for i=1:tmax
 
 endfor;
 
+%%%%VOLTAGE REGULATOR
+
+%solving using accurate model
+
+function f=f(v,Vs,R)
+Is=1e-14;
+VT=0.02586491702;
+f=v+R*Is*(exp(v/(22*VT))-1)-Vs;
+endfunction
+
+function fd=fd(v,R)
+Is=1e-14;
+VT=0.02586491702;
+fd=1+R*Is/(22*VT)*exp(v/(22*VT));
+endfunction
+
+function find_Root=Solve_f(Vs,R)
+	delta=1e-6;
+	x_next=0.5;
+
+	do
+		x=x_next;
+		x_next=x-f(x,Vs,R)/fd(x,R);
+	until (abs(x-x_next)<delta);
+	find_Root=x_next;
+endfunction
 
 
+for i=1:tmax
+v_out(i)=Solve_f(venvelope(i),R2);
+endfor;
 
 %GRAFICOS
 
+%making new vectores
+
+for j=1:2e4
+	tgraf(j)=t(j+2000);
+	v2graf(j)=v2(j+2000);
+	v3graf(j)=v3(j+2000);
+	vograf(j)=vo(j+2000);
+	venvelopegraf(j)=venvelope(j+2000);
+	v_outgraf(j)=v_out(j+2000);
+endfor
+
 hf = figure ();
-plot (t, v2, "g");
+plot (tgraf, v2graf, "g");
 hold on;
-plot (t, v3, "r");
+plot (tgraf, v3graf, "r");
 hold on;
-plot (t, vo, "b");
+plot (tgraf, vograf, "b");
 hold on;
-plot (t, venvelope, "-k");
+plot (tgraf, venvelopegraf, "-k");
 
-
+axis([tgraf(1),tgraf(2e4)]);
 xlabel ("t[s]");
 ylabel ("v2(t) [V]");
 print (hf, "v2.eps", "-depsc");
+
+clf(hf);
+hf = figure();
+plot(tgraf,v_outgraf,"b");
+axis([tgraf(1),tgraf(2e4)]);
+xlabel("t[s]");
+ylabel("v_out(t) [V]");
+print(hf,"vout.eps","-depsc");
+
+mean(venvelopegraf)
